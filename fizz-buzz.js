24eef3a2398016c422e237d00e8a53e30ad178fc
Jiizen <lady.jiizen@gmail.com>
2017-06-28 23:11:31 +0200

    Fizz Buzz
    Exercice #1 : Fizz Buzz

---
+++ fizz-buzz.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Jiizen on 28.06.2017.
+ */
+
+// Exercice Fizz Buzz : https://js.checkio.org/mission/fizz-buzz/
+
+function fizzBuzz(nb) {
+    if(typeof nb === 'number') {
+        if ((nb % 3 === 0) && (nb % 5 === 0)) {
+            return "Fizz Buzz";
+        }
+        else if (nb % 3 === 0) {
+            return "Fizz";
+        }
+        else if (nb % 5 === 0) {
+            return "Buzz";
+        }
+        else {
+            return nb.toString();
+        }
+    } else {
+        return "Veuillez rentrer un nombre correct !";
+    }
+}
+
+console.log(fizzBuzz(15));
+console.log(fizzBuzz(6));
+console.log(fizzBuzz(5));
+console.log(fizzBuzz(7));
+console.log(fizzBuzz(true));
