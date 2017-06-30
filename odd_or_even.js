fe3b4ea362c0e4c71daea46fe34cae548b2c365c
Jiizen <lady.jiizen@gmail.com>
2017-06-30 22:48:30 +0200

    Odd or even
    Odd or even

---
+++ odd_or_even.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Jiizen on 30.06.2017.
+ */
+
+// Is The Number Even Or Odd : https://edabit.com/challenge/kuzB5CMXiKDEYKXAP
+
+function isEvenOrOdd(num) {
+    if(num % 2 === 0) {
+        return 'even';
+    } else {
+        return 'odd';
+    }
+}
