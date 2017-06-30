4b8fb1f06a2a995fbfe86fbd28f609ffece19bf2
Jiizen <lady.jiizen@gmail.com>
2017-06-30 22:41:46 +0200

    Last item array
    Return The Last Item In An Array

---
+++ last_item_array.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Jiizen on 30.06.2017.
+ */
+
+// Return The Last Item In An Array :  https://edabit.com/challenge/7JBTN4TbaxJQMdX9W
+
+function getLastItem(arr) {
+    return arr[arr.length - 1];
+}
+
