4f74affaf841c28bb790f896bb165ca55dbdbd65
Jiizen <lady.jiizen@gmail.com>
2017-07-02 21:40:51 +0200

    Number prime
    Is The Number A Prime?

---
+++ number_prime.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Jiizen on 01.07.2017.
+ */
+
+// Is The Number A Prime? - https://edabit.com/challenge/oT6fy9Zvtmsnzdcvs
+
+function isPrime(num){
+    if(num === 1) {
+        return false;
+    } else {
+        for(var i = num - 1; i > 0; i--) {
+            if(num % i === 0 && i !== 1) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
