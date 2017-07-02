725f6c9c282cd232a9cdcd3256eac217e52249bd
Jiizen <lady.jiizen@gmail.com>
2017-07-02 21:42:28 +0200

    Add num === 0
    Add num === 0

--- number_prime.js
+++ number_prime.js
@@ -5,7 +5,7 @@
 // Is The Number A Prime? - https://edabit.com/challenge/oT6fy9Zvtmsnzdcvs
 
 function isPrime(num){
-    if(num === 1) {
+    if(num === 1 || num === 0) {
         return false;
     } else {
         for(var i = num - 1; i > 0; i--) {
